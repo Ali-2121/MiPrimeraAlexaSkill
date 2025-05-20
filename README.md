5d7f17fa2c940c7520e4c7387e084fc96905a2de
Alina Bonilla Paredes <prds20bnll@gmail.com>
2025-05-19 19:08:53 -0600

    Add: Añadiendo documentación en readme de evidencia de la practica y carpetas generadas en amazon developer

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# MiPrimeraAlexaSikill
-Skill básica de Alexa llamada "Saludo Alina" que responde quién creó la app, qué carrera estudia, su color favorito y su grupo o cantante favorito.
+<div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 20px;">
+  <img src="./imgs/logo_tic.png" alt="Imagen izquierda" >
+  <img src="./imgs/logo_utxj.png" alt="Imagen derecha">
+</div>
+
+## UNIVERSIDAD TECNOLÓGICA DE XICOTEPEC DE JUÁREZ 
+
+**MATERIA:** Extracción de Conocimiento de Bases de Datos  
+**ALUMNA:** Alina Bonilla Paredes  
+**MATRÍCULA:** 220385  
+
+### Práctica 03: Funcionalidades por Comandos de Voz
+
+Este repositorio contiene el código fuente de una skill básica para Alexa, desarrollada a través de la plataforma Amazon Developer. La invocación de la skill es **"Saludo Alina"**.
+
+---
+
+### 🗣️ ¿Qué hace esta skill?
+
+Una vez invocada, la skill está diseñada para responder a las siguientes preguntas de manera personalizada:
+
+- ¿Quién creó la aplicación?
+- ¿Qué carrera estudia?
+- ¿Cuál es su color favorito?
+- ¿Cuál es su grupo o cantante favorito?
+
+---
+
+### 🛠️ Tecnologías utilizadas
+
+- AWS Lambda (Node.js)
+- Alexa Skills Kit (ASK)
+- Amazon Developer Console
+
+---
+
+### Imágenes
+
+![Imagen 3](./imgs/invocacion.png)  
+![Imagen 4](./imgs/3preguntas.png)
+![Imagen 5](./imgs/2preguntas.png)
+![Imagen 6](./imgs/preguntas.png)
---
+++ lambda/index.js
@@ -0,0 +1,172 @@
+/* *
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * */
+const Alexa = require('ask-sdk-core');
+
+const SaludoIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SaludoIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hola, saludos desde la universidad tecnológica de Xicotepec de Juárez.';
+return handlerInput.responseBuilder.speak(speakOutput).reprompt('Puedes preguntarme quién creó esta skill, qué carrera estudia, cual es su color favorito o cual es su musica favorita.').getResponse();
+    }
+};
+
+const InfoIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'InfoIntent';
+    },
+    handle(handlerInput) {
+        const infoSlot = Alexa.getSlotValue(handlerInput.requestEnvelope, 'infoTipo');
+        
+        let speakOutput = '';
+
+        switch (infoSlot) {
+            case 'creo':
+                speakOutput = 'Esta skill fue creada por Alina Bonilla Paredes.';
+                break;
+            case 'carrera':
+                speakOutput = 'Alina estudia el noveno cuatrimestre de la Ingeniería en Desarrollo de Software Multiplataforma.';
+                break;
+            case 'color favorito':
+                speakOutput = 'El color favorito de Alina es el rosa pastel.';
+                break;
+            case 'grupo o cantante favorito':
+                speakOutput = 'A Alina le gusta la música de Adele y Carla Morrison, aunque escucha de todo.';
+                break;
+            default:
+                speakOutput = 'No tengo información sobre eso. Puedes preguntarme quien creó la aplicación, qué carrera estudia, cuál es su su color favorito o su grupo o cantante favorito.';
+        }
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt('¿Te gustaría saber otra cosa sobre Alina?')
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+/*const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};*/
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        SaludoIntentHandler,
+        InfoIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withApiClient(new Alexa.DefaultApiClient()) 
+    .withCustomUserAgent('sample/hello-world/v1.2')
+    .lambda();
